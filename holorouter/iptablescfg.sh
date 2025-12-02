b147618aef6155c40dae400f20b0194552bcfee0
Burke Azbill <burkeazbill@users.noreply.github.com>
2025-12-02 08:10:36 -0500

    Add packages repo access to override

--- holorouter/iptablescfg.sh
+++ holorouter/iptablescfg.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
-# DEFAULT HOL FIREWALL RULESET
-# version 02-July 2025
+# Override HOL FIREWALL RULESET
+# version 02-December 2025
 
 # clear any existing rules
 iptables --flush
@@ -120,6 +120,13 @@ iptables -A FORWARD -s 172.16.0.0/12 -d 10.0.0.0/8      -j ACCEPT
 iptables -A FORWARD -s 10.0.0.0/8 -d 192.168.0.0/16     -j ACCEPT
 iptables -A FORWARD -s 10.0.0.0/8 -d 172.16.0.0/12      -j ACCEPT
 iptables -A FORWARD -s 10.0.0.0/8 -d 10.0.0.0/8         -j ACCEPT
+
+# Allow access to Broadcom package repository
+#This new rule automatically resolves the domain name projects.packages.broadcom.com to its current IP addresses and adds a corresponding iptables ACCEPT rule for each IP address in the FORWARD chain.
+for ip in $(dig +short projects.packages.broadcom.com | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
+do
+   iptables -A FORWARD -d $ip -j ACCEPT
+done
 
 ### LAB-SPECIFIC RULES
 
